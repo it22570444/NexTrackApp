ed831b88b064aaaaeca7bd1d69d8170b78f2492f
Navishka Gunawardana <128272519+it22570444@users.noreply.github.com>
2024-05-12 14:57:07 +0530

    colors change

--- app/src/main/java/com/example/nextrackapp/AddNoteActivity.kt
+++ app/src/main/java/com/example/nextrackapp/AddNoteActivity.kt
@@ -6,17 +6,17 @@ import android.widget.Toast
 import com.example.nextrackapp.databinding.ActivityAddNoteBinding
 
 class AddNoteActivity : AppCompatActivity() {
-    
+
     private lateinit var binding: ActivityAddNoteBinding
     private lateinit var db: NotesDatabaseHelper
-    
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityAddNoteBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        
+
         db = NotesDatabaseHelper(this)
-        
+
         binding.saveButton.setOnClickListener {
             val title = binding.titleEditText.text.toString()
             val content = binding.contentEditText.text.toString()
